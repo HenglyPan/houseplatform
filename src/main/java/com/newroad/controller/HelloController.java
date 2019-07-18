5efd82cbede4088d6870934378c96175256e76d3
HenglyPan <1521388510@qq.com>
2019-07-18 22:27:28 +0800

    整合freemarker druid

--- src/main/java/com/newroad/controller/HelloController.java
+++ src/main/java/com/newroad/controller/HelloController.java
@@ -1,14 +1,11 @@
 package com.newroad.controller;
-
 import com.newroad.entity.User;
 import com.newroad.service.UserServiceIf;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import java.util.List;
-
 @Controller
 public class HelloController {
     @Autowired
@@ -23,7 +20,7 @@ public class HelloController {
     }
 
     @RequestMapping("index")
-    
+
     public String index(){
         return "homepage/index";
 
